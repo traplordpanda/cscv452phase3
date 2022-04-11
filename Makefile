3052c405582e86ae0dd239600e5cc3244f5416bf
traplordpanda <kdaub.tf2@gmail.com>
2022-04-11 19:22:31 -0400

    initial comit

---
+++ Makefile
@@ -0,0 +1,41 @@
+TARGET=libphase3.a
+ASSIGNMENT=452phase3
+CC=gcc
+AR=ar
+COBJS= phase3.o 
+CSRCS=${COBJS:.o=.c}
+HDRS= sems.h
+
+INCLUDE = ./usloss/include
+CFLAGS = -Wall -g -I${INCLUDE} -I. -Wno-int-to-pointer-cast -Wno-pointer-to-int-cast
+
+
+LDFLAGS = -L. -L./usloss/lib
+TESTDIR=testcases
+TESTS= test00 test01 test02 test03 test04 test05 test06 test07 test08 \
+       test09 test10 test11 test12 test13 test14 test15 test16 test17 \
+       test18 test19 test20 test21 test22 test23 test24 test25 
+
+# Use one of the following LIBS lines, depending on whose phase1/2 you are using
+# lxu’s phase2 and phase1
+LIBS = -llxuphase2 -llxuphase1 -lusloss -llxuphase1\
+       -llxuphase2 -lphase3 
+# Your phase1 and your phase2
+#LIBS = -lphase2 -lphase1 -lusloss -luser 
+
+
+
+$(TARGET):	$(COBJS)
+		$(AR) -r $@ $(COBJS) 
+
+$(TESTS):	$(TARGET)  p1.o libuser.o
+	$(CC) $(CFLAGS) -c $(TESTDIR)/$@.c
+	$(CC) $(CFLAGS) -c libuser.c
+	$(CC) $(LDFLAGS) -o $@ $@.o $(LIBS) p1.o libuser.o
+
+clean:
+	rm -f $(COBJS) $(TARGET) test*.o term* $(TESTS) libuser.o p1.o core
+
+phase3.o:	sems.h
+
+
---
+++ skeleton.c
@@ -0,0 +1,56 @@
+#include <usloss.h>
+#include <phase1.h>
+#include <phase2.h>
+#include <phase3.h>
+
+int start2(char *); 
+int  spawn_real(char *name, int (*func)(char *), char *arg,
+                int stack_size, int priority);
+int  wait_real(int *status);
+int
+start2(char *arg)
+{
+    int		pid;
+    int		status;
+    /*
+     * Check kernel mode here.
+     */
+
+    /*
+     * Data structure initialization as needed...
+     */
+
+
+    /*
+     * Create first user-level process and wait for it to finish.
+     * These are lower-case because they are not system calls;
+     * system calls cannot be invoked from kernel mode.
+     * Assumes kernel-mode versions of the system calls
+     * with lower-case names.  I.e., Spawn is the user-mode function
+     * called by the test cases; spawn is the kernel-mode function that
+     * is called by the syscall_handler; spawn_real is the function that
+     * contains the implementation and is called by spawn.
+     *
+     * Spawn() is in libuser.c.  It invokes usyscall()
+     * The system call handler calls a function named spawn() -- note lower
+     * case -- that extracts the arguments from the sysargs pointer, and
+     * checks them for possible errors.  This function then calls spawn_real().
+     *
+     * Here, we only call spawn_real(), since we are already in kernel mode.
+     *
+     * spawn_real() will create the process by using a call to fork1 to
+     * create a process executing the code in spawn_launch().  spawn_real()
+     * and spawn_launch() then coordinate the completion of the phase 3
+     * process table entries needed for the new process.  spawn_real() will
+     * return to the original caller of Spawn, while spawn_launch() will
+     * begin executing the function passed to Spawn. spawn_launch() will
+     * need to switch to user-mode before allowing user code to execute.
+     * spawn_real() will return to spawn(), which will put the return
+     * values back into the sysargs pointer, switch to user-mode, and 
+     * return to the user code that called Spawn.
+     */
+    pid = spawn_real("start3", start3, NULL, 4*USLOSS_MIN_STACK, 3);
+    pid = wait_real(&status);
+
+} /* start2 */
+
